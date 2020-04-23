f0eed1bb0288fd34c19e8dd74395c84cf5efe3a3
Dmitry Sychev <axiumone@gmail.com>
2020-04-23 17:21:07 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,7 +1,10 @@
-function forLoop(){
   const array = [1, 2]
+
+function forLoop(){
   const t = 25
   for (let i = 0; i < t; i++) {
     array.push("I am a strange loop.")
   }
 }
+
+forLoop(array)
