d8df76d412b5c6f3f6c74d4bcaf5a27d099ec52e
Dmitry Sychev <axiumone@gmail.com>
2020-04-23 17:27:08 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -2,7 +2,7 @@ let gifts = ["teddy bear", "drone", "doll"];
 
 function forLoop(gifts) {
   for (let i = 0; i < 25; i++) {
-    console.log(`I am a strange loop.`);
+    console.log(`I am ${i} strange loop.`);
   }
 }
 
