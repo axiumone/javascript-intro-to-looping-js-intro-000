131e6180f82ef1922c68f301633e23c64eb6368b
Dmitry Sychev <axiumone@gmail.com>
2020-04-23 17:23:33 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,9 +1,9 @@
-let t = [1,2,3,4,5,6]
+let t = [1,2,3,4,5,6];
 
 function forLoop(){
 
   for (let i = 0; i < t; i++) {
-    array.push("I am a strange loop.")
+    array.push("I am a strange loop.");
   }
   return [array, t]
 }
